f9e90aaf79a538e6400d1295b22aed39114f7b3b
YuriR01 <yuriraggiotto@hotmail.com>
2022-05-05 11:35:54 -0300

    IceDamage
    possui: FireDamage and IceDamage
    absent: RayDamage

---
+++ Assets/Scripts/Damage/IceDamageController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MovementCommands;
+using UnityEngine;
+
+namespace Damage
+{
+    public class IceDamageController : MonoBehaviour
+    {
+        [SerializeField] private NormalMovementCommand _normalMovementCommand;
+        [SerializeField] private Health health;
+        public string damageString;
+        public float bulletIceDamage;
+        [SerializeField]public bool isOnIce;
+        [SerializeField] private float iceDelay = 0.1f;
+        public float slow;
+        
+
+        private IEnumerator ResetIce()
+        {
+            yield return new WaitForSeconds(iceDelay);
+            isOnIce = true;
+        }
+
+        //private void Update()
+        //{
+            //if (isOnIce)
+            //{
+               // health.RemoveHp(damage);
+            //}
+            
+        //}
+        private void Update()
+        {
+            if (isOnIce)
+            {
+                NormalMovementCommand.speed = 0.5f; 
+                isOnIce = false;
+            }
+        }
+
+        private void OnEnable()
+        {
+            health.TookElementalDamageEvent += OnElementalDamage;
+        }
+        private void OnDisable()
+        {
+            health.TookElementalDamageEvent -= OnElementalDamage;
+        }
+        
+        private void OnElementalDamage(ElementalDamage elementalDamage)
+        {
+            if (elementalDamage.nome == damageString)
+            {
+                isOnIce = true;
+                StartCoroutine(ResetIce());
+            }
+            
+        }
+        
+    }
+}
--- Assets/Scripts/MovementCommands/NormalMovementCommand.cs
+++ Assets/Scripts/MovementCommands/NormalMovementCommand.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
 
 namespace MovementCommands
 {
     [CreateAssetMenu(menuName = "Create NormalMovementCommand", fileName = "NormalMovementCommand", order = 0)]
-    public class NormalMovementCommand : MovementCommand
+    public class NormalMovementCommand : MovementCommand 
     {
-        public float speed;
+        public static float speed;
         public override void Move(Rigidbody2D character, Vector2 direction)
         {
             character.transform.Translate(direction*speed*Time.deltaTime);
+        }
+    }
+    public class IceMovementCommand : MovementCommand
+    {
+        public override void Move(Rigidbody2D rigidbody2D, Vector2 direction)
+        {
+            NormalMovementCommand.speed = 0.5f;
         }
     }
 }
